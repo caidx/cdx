66a7fc33c23decaa937445a5c947cca09c226b01
cdx <2014347001@qq.com>
2019-12-18 16:17:48 +0800

    新增cdx.py

---
+++ cdx.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#!/bin/env python
+# Time: Wed 18 Dec 2019 04:16:10 PM CST
+# Description:
+# File Name: cdx.py
+
+def test():
+    print('Hello Wrold')
+    print('new test.py file')
